2bf40396d9ab03de62421f14ef4abf9ea584598c
Roheet <roheet.n@gmail.com>
2021-12-25 18:42:52 +0100

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,14 +1,18 @@
 from pathlib import Path
 import datetime
 import pytz
-
+import requests
 
 def update_footer():
+    url = "https://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote"
+    response = requests.request("GET",url,verify=False)
+    quote = response.json()["content"]
+    readme = Path('../README.md').read_text()
     timestamp = datetime.datetime.now(pytz.timezone("Europe/Berlin")).strftime("%c")
     footer = Path('../FOOTER.md').read_text()
-    return footer.format(timestamp=timestamp)
+    return footer.format(timestamp=timestamp,quote=quote)
 
-readme = Path('../README.md').read_text()
+
 
 with open('../README.md', "w+") as f:
     f.write(readme[:readme.find("<hr>")] + update_footer())
